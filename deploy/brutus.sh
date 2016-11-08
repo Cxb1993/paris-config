97862cd4098e34d5bae303a19842cd1a5b3e49da
Sergey Litvinov <slitvinov@gmail.com>
2016-11-08 17:58:21 +0100

    Create a version of deploy script for brutus

--- deploy/brutus.sh
+++ deploy/brutus.sh
@@ -1,79 +1,85 @@
 #!/bin/bash
 
-# Install paris and third party libraries
+set -eu
+ROOT=$HOME/paris-deploy
+PREFIX=$ROOT/prefix
+SRC=$ROOT/src
+
+env () { # set env on brutus
+    module load open_mpi
+    export PATH=$PATH:$PREFIX/bin
+}
 
 install_openmpi () {
-    (
-	va=2.0 vb=1 # major an minor version
-	wget https://www.open-mpi.org/software/ompi/v${va}/downloads/openmpi-${va}.${vb}.tar.gz
-	tar zxvf openmpi-${va}.${vb}.tar.gz
-	cd openmpi-${va}.${vb}
-	./configure --prefix=$HOME/prefix/openmpi --enable-mpi-fortran
-	make
-	make install
-    )
+    va=2.0 vb=1 # major an minor version
+    wget https://www.open-mpi.org/software/ompi/v${va}/downloads/openmpi-${va}.${vb}.tar.gz
+    tar zxvf openmpi-${va}.${vb}.tar.gz
+    cd openmpi-${va}.${vb}
+    ./configure --prefix=$PREFIX/openmpi --enable-mpi-fortran
+    make
+    make install
 }
 
 install_vofi () {
-    (
-	v=1.0
-	wget http://www.ida.upmc.fr/~zaleski/paris/Vofi-${v}.tar.gz
-	tar zxvf Vofi-${v}.tar.gz
-	cd Vofi
-	./configure --prefix=$HOME/prefix/vofi
-	make
-	make install
-    )
+    v=1.0
+    wget http://www.ida.upmc.fr/~zaleski/paris/Vofi-${v}.tar.gz
+    tar zxvf Vofi-${v}.tar.gz
+    cd Vofi
+    ./configure --prefix=$PREFIX/vofi
+    make
+    make install
 }
 
 install_hypre () {
-    (
-	v=2.11.1
-	wget http://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-${v}.tar.gz
-	tar zxvf hypre-${v}.tar.gz
-	cd hypre-${v}/src
-	./configure --prefix=$HOME/prefix/hypre
-	make
-	make install
-    )
+    v=2.11.1
+    wget http://computation.llnl.gov/projects/hypre-scalable-linear-solvers-multigrid-methods/download/hypre-${v}.tar.gz
+    tar zxvf hypre-${v}.tar.gz
+    cd hypre-${v}/src
+    ./configure --prefix=$PREFIX/hypre
+    make
+    make install
 }
 
 install_silo () {
-    (
-	v=4.10.2
-	wget https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-${v}/silo-${v}.tar.gz
-	tar zxvf silo-${v}.tar.gz
-	cd silo-${v}
-	./configure --prefix=$HOME/prefix/silo
-	make
-	make install
-    )
+    v=4.10.2
+    wget https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-${v}/silo-${v}.tar.gz
+    tar zxvf silo-${v}.tar.gz
+    cd silo-${v}
+    ./configure --prefix=$PREFIX/silo
+    make
+    make install
 }
 
 install_paris () {
-    git clone git://github.com/slitvinov/paris-git --branch cse
+    # git clone git://github.com/slitvinov/paris-git --branch cse
     cd paris-git
 
+    mkdir -p $PREFIX/paris/bin
     make FLAGS="-O3 -cpp  -fimplicit-none" \
 	 HAVE_VOFI=1 HAVE_SILO=1 \
-	 SILO_DIR=$HOME/prefix/silo \
-	 HYPRE_DIR=$HOME/prefix/hypre/lib \
-	 VOFI_DIR=$HOME/prefix/vofi/lib
+	 SILO_DIR=$PREFIX/silo \
+	 HYPRE_DIR=$PREFIX/hypre/lib \
+	 VOFI_DIR=$PREFIX/vofi/lib \
+	 BINDIR=$PREFIX/paris/bin
 }
 
 install_tools () {
+    git clone git@gitlab.ethz.ch:mavt-cse/paris-cse.git
+    cd paris-cse
+    
     (cd tools/wparis ; ./install.sh)
     (cd tools/bubbles; ./install.sh)
 }
 
-mkdir -p paris-src
-cd       paris-src
+force_cd () { # create a directory before `cd'
+    mkdir -p "$1"
+    cd       "$1"
+}
 
-#install_openmpi
-#export PATH=$HOME/prefix/openmpi/bin:$PATH
-
-install_vofi &&
-    install_hypre &&
-    install_silo &&
-    install_paris &&
-    install_tools
+env
+force_cd "$SRC"
+#(install_vofi)
+#(install_hypre)
+#(install_silo)
+(install_paris)
+#(install_tools)
