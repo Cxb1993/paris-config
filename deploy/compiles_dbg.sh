0f88a17a452a48d0f168c07dad6f55ce25d498e2
Sergey Litvinov <slitvinov@gmail.com>
2017-05-13 01:26:49 +0200

    Remove unused file

--- deploy/compiles_dbg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-make FLAGS="-O0 -g -cpp  -fimplicit-none" \
-     HAVE_VOFI=1 HAVE_SILO=1 \
-     SILO_DIR=$HOME/prefix/silo \
-     HYPRE_DIR=$HOME/prefix/hypre/lib \
-     VOFI_DIR=$HOME/prefix/vofi/lib \
-     BINDIR=$HOME/prefix/paris_dbg
-
