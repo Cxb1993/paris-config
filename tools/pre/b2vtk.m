5fddf16bab34ca69dd98cf0a9fa8f8a9269ca79c
Visitor CSElab <slitvinov@gmail.com>
2017-05-17 17:53:41 +0200

    Add rbackup_header

--- tools/pre/b2vtk.m
+++ tools/pre/b2vtk.m
@@ -2,13 +2,19 @@
 
 1; # make it a script
 
-function rbackup_header(fd)
+function i = header(fd)
   l = fgets(fd);
-  [time, timestamp, imin, imax, jmin, jmax, kmin, kmax] = ...
+  [i.time, i.timestamp, i.imin, i.imax, i.jmin, i.jmax, i.kmin, i.kmax] = ...
      strread(l, '%f %d    %d %d %d   %d %d %d');
 endfunction
 
-function rbackup_data(fd)
+function i = header_file(f)
+  fd = fopen(f, 'r');
+  i = header(fd);
+  fclose(fd);
+endfunction
+
+function data(fd)
   nvar = 3 + 1 + 1; # u, v, w, p, cvof
   buf = fscanf(fd, '%f');
   buf = reshape(buf, nvar, []);
@@ -17,10 +23,30 @@ endfunction
 
 function rbackup(f)
   fd = fopen(f, 'r');
-  rbackup_header(fd);
-  rbackup_data(fd);
+  header(fd);
+  data(fd);
   fclose(fd);
 endfunction
 
-f = 'test_data/np8cube/backup_00000'
-rbackup(f)
+function g = maxmin(g, i)
+  g.imin = min(g.imin, i.imin);
+  g.jmin = min(g.jmin, i.jmin);
+  g.kmin = min(g.kmin, i.kmin);
+
+  g.imax = max(g.imax, i.imax);
+  g.jmax = max(g.jmax, i.jmax);
+  g.kmax = max(g.kmax, i.kmax);
+endfunction
+
+function ginfo = limits(fl) # get global domain info
+  for ia=1:numel(fl); f = fl{ia};
+      info = header_file(f);
+      if ia == 1; ginfo = info; endif
+      ginfo = maxmin(ginfo, info); # [g]lobal [i]nfo
+  endfor
+endfunction
+
+f     = 'test_data/np8cube/backup_00000';
+ginfo = limits(argv())
+
+# rbackup(f)
