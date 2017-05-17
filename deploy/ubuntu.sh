3ce97f1e545077ed03b9445db5faa774c9898429
Visitor CSElab <slitvinov@gmail.com>
2017-05-17 12:45:52 +0200

    minor

--- deploy/ubuntu.sh
+++ deploy/ubuntu.sh
@@ -6,13 +6,13 @@ set -eu
 . deploy/ubuntu.inc.sh
 . deploy/.parisrc
 
-# Installing third party libraries
+# download and install third party libraries
 install_openmpi
 install_vofi
 install_hypre
 install_silo
 
-# Install CSE tools
+# download and install CSE tools
 install_tools
 
 clone_paris
