c96fabadc7047b30adc82d6f8e37d5c5c8babfda
Sergey Litvinov <slitvinov@gmail.com>
2017-05-18 12:04:02 +0200

    Write binary file

--- tools/pre/b2vtk.m
+++ tools/pre/b2vtk.m
@@ -1,27 +1,31 @@
 #!/usr/bin/env octave-qf
 
-global NVAR;
-global u; global v; global w;
-global p; global cvof;
+global u v w p cvof
+global NVAR
 NVAR = 3        + 1 + 1;
+global fd # file descriptor
 
-function i = header(fd)
+function i = header()
+  global fd
   l = fgets(fd);
   [i.time, i.timestamp, i.imin, i.imax, i.jmin, i.jmax, i.kmin, i.kmax] = ...
      strread(l, '%f %d    %d %d %d   %d %d %d');
 endfunction
 
 function i = header_file(f)
+  global fd
   fd = fopen(f, 'r');
   i = header(fd);
   fclose(fd);
 endfunction
 
-function buf = data(fd)
+function buf = data()
+  global fd
   buf = fscanf(fd, '%f');
 endfunction
 
 function [info, buf] = rbackup(f)
+  global fd
   fd = fopen(f, 'r');
   info = header(fd);
   buf  = data(fd);
@@ -66,22 +70,26 @@ u0   = reshape(u0,
 u(i.imin:i.imax, i.jmin:i.jmax, i.kmin:i.kmax) = u0;
 ####
 
-function vtk_version(fd)
+function vtk_version()
+  global fd
   p = @(varargin) fprintf(fd, varargin{:});
   p("# vtk DataFile Version 2.0\n");
 endfunction
 
-function vtk_header(fd)
+function vtk_header()
+  global fd
   p = @(varargin) fprintf(fd, varargin{:});
   p("Created with b2vtk.m\n");
 endfunction
 
-function vtk_format(fd)
+function vtk_format()
+  global fd
   p = @(varargin) fprintf(fd, varargin{:});
   p("BINARY\n"); % ASCII
 endfunction
 
-function vtk_topo(fd)
+function vtk_topo() # topology
+  global fd
   p = @(varargin) fprintf(fd, varargin{:});
   global u
   X = 1; Y = 2; Z = 3;
@@ -94,28 +102,27 @@ function vtk_topo(fd)
   p("SPACING %17.8g %17.8g %17.8g\n", s(X), s(Y), s(Z));
 endfunction
 
-function vtk_pdata_header(fd)
+function vtk_pdata_header()
+  global fd
   p = @(varargin) fprintf(fd, varargin{:});
   global u
   p("POINT_DATA %d\n", numel(u));
 endfunction
 
-function vtk_scalar(fd)
+function vtk_scalar()
+  global fd u
   p = @(varargin) fprintf(fd, varargin{:});
-  global u
-  p("SCALARS %s %s\n", "u", "double")
+  type = "double";
+  p("SCALARS %s %s\n", "u", type)
   p("LOOKUP_TABLE default\n")
   ##  dlmwrite(fd, u(:), ' ');
-  skip = 0
-  fwrite(fd, u, "double", skip, "ieee-be");
+  byte_skip = 0; arch = "ieee-be"; 
+  fwrite(fd, u, type, byte_skip, arch);
 endfunction
 
 fo = "o.vtk";
 fd = fopen(fo, "w");
-vtk_version(fd);
-vtk_header(fd);
-vtk_format(fd);
+vtk_version(fd); vtk_header(fd); vtk_format(fd);
 vtk_topo(fd);
-vtk_pdata_header(fd);
-vtk_scalar(fd);
+vtk_pdata_header(fd); vtk_scalar(fd);
 fclose(fd);
